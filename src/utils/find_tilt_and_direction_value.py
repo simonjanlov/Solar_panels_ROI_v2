b0eeb2b084b4ab376a4eaf5814539afaa66db2a0
Simon Janlöv <simon.janlov@gmail.com>
2023-11-20 15:01:02 +0100

    another path update

--- src/utils/find_tilt_and_direction_value.py
+++ src/utils/find_tilt_and_direction_value.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from pathlib import Path
 
 
 def find_tilt_and_direction_value(tilt, direction):
     """Input tilt as an integer and direction as a string to return a
     float value for combined tilt and direction"""
-    
-    df = pd.read_csv(r'data/tilt_and_direction_table.csv', delimiter=';', index_col='Lutning')
+    csv_path = Path(__file__).resolve().parents[2] / 'data' / 'tilt_and_direction_table.csv'
+    df = pd.read_csv(csv_path, delimiter=';', index_col='Lutning')
     return df[direction][tilt] / 100
 
 
